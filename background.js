086f952aec0d65bc291c23b028e1dfc3607297ae
Dennis <>
2026-06-27 00:07:05 -0400

    redirect preview.redd.it to i.redd.it

--- background.js
+++ background.js
@@ -1,61 +1,90 @@
 // Create list of rules
 const headerRules = [
+  // Redirect preview.redd.it to i.redd.it
   {
-    "id" : 1,
-    "priority": 1,
-    "action" : {
-      "type" : "modifyHeaders",
-      "requestHeaders": [{
-        "header": "Accept",
-        "operation": "remove"
-      }]
+    id: 1,
+    priority: 2,
+    action: {
+      type: "redirect",
+      redirect: {
+        transform: {
+          host: "i.redd.it"
+        }
+      }
     },
-    "condition" : {
-      "urlFilter" : "i.redd.it",
-      "resourceTypes": ["main_frame", "sub_frame"]
+    condition: {
+      urlFilter: "preview.redd.it",
+      resourceTypes: ["main_frame"]
     }
   },
+
+  // Remove the Accept header for i.redd.it
   {
-    "id" : 2,
-    "priority": 1,
-    "action" : {
-      "type" : "modifyHeaders",
-      "requestHeaders": [{
-        "header": "Accept",
-        "operation": "remove"
-      }]
+    id: 2,
+    priority: 1,
+    action: {
+      type: "modifyHeaders",
+      requestHeaders: [
+        {
+          header: "Accept",
+          operation: "remove"
+        }
+      ]
     },
-    "condition" : {
-      "urlFilter" : "preview.redd.it",
-      "resourceTypes": ["main_frame", "sub_frame"]
+    condition: {
+      urlFilter: "i.redd.it",
+      resourceTypes: ["main_frame"]
     }
   },
+
+  // Remove the Accept header for preview.redd.it
   {
-    "id" : 3,
-    "priority": 1,
-    "action" : {
-      "type" : "modifyHeaders",
-      "requestHeaders": [{
-        "header": "Accept",
-        "operation": "remove"
-      }]
+    id: 3,
+    priority: 1,
+    action: {
+      type: "modifyHeaders",
+      requestHeaders: [
+        {
+          header: "Accept",
+          operation: "remove"
+        }
+      ]
     },
-    "condition" : {
-      "urlFilter" : "external-preview.redd.it",
-      "resourceTypes": ["main_frame", "sub_frame"]
+    condition: {
+      urlFilter: "preview.redd.it",
+      resourceTypes: ["main_frame"]
+    }
+  },
+
+  // Remove the Accept header for external-preview.redd.it
+  {
+    id: 4,
+    priority: 1,
+    action: {
+      type: "modifyHeaders",
+      requestHeaders: [
+        {
+          header: "Accept",
+          operation: "remove"
+        }
+      ]
+    },
+    condition: {
+      urlFilter: "external-preview.redd.it",
+      resourceTypes: ["main_frame"]
     }
   }
-]
+];
 
 // Enable the rules
 chrome.runtime.onInstalled.addListener(() => {
   // First, remove any existing rules with the same IDs
   chrome.declarativeNetRequest.updateDynamicRules({
-    removeRuleIds: [1, 2, 3]
-  }, () => {
-    // Then add the new rules
-    chrome.declarativeNetRequest.updateDynamicRules({
-      addRules: headerRules
+      removeRuleIds: [1, 2, 3, 4]
+    }, () => {
+      // Then add the new rules
+      chrome.declarativeNetRequest.updateDynamicRules({
+        addRules: headerRules
+      });
     });
-  });
 });
